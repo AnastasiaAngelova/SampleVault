cb8cfd35c73a8b0342b671c8e2af548d90d68e12
angelova <angelova.03@mail.ru>
2024-04-26 17:12:19 +0300

    signup fix

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import LeftMenu from './components/LeftMenu/LeftMenu';
-// import MainContent from './components/MainContent/MainContent';
-// import Player from './components/Player/Player';
 import { PlaylistProvider } from "./components/Player/PlaylistContext";
 import AudioControler from "./components/Player/Player";
 import Feed from './containers/Feed/Feed'
 import SoundGen from './containers/SoundGen/SoundGen'
 import SoundCollection from './containers/SoundCollection/SoundCollection';
 import Signup from './containers/Signup/Signup'
-import ReactAudioPlayer from 'react-audio-player';
 
 function App() {
+    // Проверяем текущий путь
+    const currentPath = window.location.pathname;
+
+    if (currentPath === "/signup") {
+        return (
+            <PlaylistProvider>
+                <Router>
+                    <Signup />
+                </Router>
+            </PlaylistProvider>
+        );
+    }
+
     return (
         <PlaylistProvider>
             <Router>
@@ -24,11 +34,8 @@ function App() {
                         <Route path="/feed" element={<Feed />} />
                         <Route path="/generate" element={<SoundGen />} />
                         <Route path="/sound_collection" element={<SoundCollection />} />
-                        <Route exact path="/signup" element={<Signup />} />
                     </Routes>
                 </div>
-
-                {/* <Player /> */}
                 <AudioControler />
             </Router>
         </PlaylistProvider>

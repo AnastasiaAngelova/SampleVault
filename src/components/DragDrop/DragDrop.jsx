88844545a6d45f2d2d4b2914f2b90f8da5ce4d3f
Kulitsov <kulitskovs@yandex.ru>
2024-04-27 17:30:50 +0300

    Added upload page

--- src/components/DragDrop/DragDrop.jsx
+++ src/components/DragDrop/DragDrop.jsx
@@ -9,53 +9,35 @@ function DragDrop({ fileTypes, label, dropMessageStyle, pathtoicon, alt}) {
     const [file, setFile] = useState(null);
 
     const handleChange = (file) => {
+        console.log('handle');
         setFile(file);
+        console.log(file);
       };
-
-    if (file) {
-            return(
-                <FileUploader 
-                    handleChange={handleChange} 
-                    name="file" 
-                    types={fileTypes} 
-                    label={file.name} 
-                    dropMessageStyle={dropMessageStyle}
-                    pathtoicon={pathtoicon}
-                    alt={alt}
-                    children=
-                    <div className={`generation-drag-drop-img-wrapper`}>
-                        <div className='generation-drag-drop-img-text'>
-                            {file.name}
-                        </div>
-                        <div className='generation-drag-drop-img-icon'>
-                            <img src={pathtoicon} alt={alt} />
-                        </div>
-                    </div>
-                />
-            );
-            
-        } else {  
-
-            return (
-            <FileUploader 
-                handleChange={handleChange} 
-                name="file" 
-                types={fileTypes} 
-                label={label} 
-                dropMessageStyle={dropMessageStyle}
-                pathtoicon={pathtoicon}
-                alt={alt}
-                children=
-                <div className={`generation-drag-drop-img-wrapper`}>
-                    <div className='generation-drag-drop-img-text'>
-                        {label}
-                    </div>
-                    <div className='generation-drag-drop-img-icon'>
-                        <img src={pathtoicon} alt={alt} />
-                    </div>
-                </div>
-            />
-    );}
+        
+    return (
+    <FileUploader 
+        handleChange={handleChange} 
+        name="file" 
+        types={fileTypes} 
+        {...file ? (
+            {label: file.name}
+        ) : (
+            {label: label}
+        )}
+        dropMessageStyle={dropMessageStyle}
+        pathtoicon={pathtoicon}
+        alt={alt}
+        children=
+        <div className={`generation-drag-drop-img-wrapper`}>
+            <div className='generation-drag-drop-img-text'>
+                {label}
+            </div>
+            <div className='generation-drag-drop-img-icon'>
+                <img src={pathtoicon} alt={alt} />
+            </div>
+        </div>
+    />
+    );
 }
 
 export default DragDrop;

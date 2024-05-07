f1dae3f1bbd5243c70f2d78016192aa3edbddbf6
angelova <angelova.03@mail.ru>
2024-05-07 23:38:28 +0300

    Added some changes

--- src/components/FeedContent/Cardkit/Cardkit.jsx
+++ src/components/FeedContent/Cardkit/Cardkit.jsx
@@ -4,7 +4,13 @@ import { Song } from '../Song/Song'
 import './Cardkit.css'
 
 const Cardkit = (props) => {
-
+  if (!props.trendSounds || props.trendSounds.length === 0) {
+    return (
+        <div className="cardkit-container">
+            <div className="loader22"></div>
+        </div>
+    );
+  }
 //   console.log('logging '+process.env.PUBLIC_URL)
   return (
     <div className="cardkit-container">

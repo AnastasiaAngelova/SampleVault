f89b6240da27347c380bbd422c8d00ac0596b96c
Owling797 <zavadskayalus@gmail.com>
2024-05-22 19:37:20 +0300

    cringe alt image removed

--- src/components/Navbar/DropProfile/DropProfile.jsx
+++ src/components/Navbar/DropProfile/DropProfile.jsx
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./Drop.css"
 
 
-const DropProfile = ({onMenuClick}) => {
+const DropProfile = ({ onMenuClick }) => {
     const [isOpen, setOpen] = useState(false);
 
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
 
     const handleMenuSelect = () => {
         setOpen(false);
@@ -22,34 +22,34 @@ const DropProfile = ({onMenuClick}) => {
         <div className="dr">
             <button className="menu-button" onClick={() => setOpen(!isOpen)}>
                 <div className="left">
-                <span className='txt'>Создать</span>
+                    <span className='txt'>Создать</span>
                 </div>
                 <div className="right">
-                    <img className="chevron" src={"icons/chevron/chevron-down.svg"} alt="My SVG" />
+                    <img className="chevron" src={"icons/chevron/chevron-down.svg"} alt="" />
                 </div>
             </button>
             <nav className={`menu ${isOpen ? "active" : ""}`}>
                 <ul className="menu-list">
-                   <div className="new-buttons">
+                    <div className="new-buttons">
                         <button className="btn-top" onClick={handleMenuSelect}>
-                            <span className='txt' style={{color:"black"}}>Новый звук c </span>
+                            <span className='txt' style={{ color: "black" }}>Новый звук c </span>
                             {/* <span className='txt' style={{color:"black"}}> </span> */}
-                            <span className='txt' style={{color:"red"}}>AI</span>
+                            <span className='txt' style={{ color: "red" }}>AI</span>
                         </button>
                         <button className="btn-bottom">
-                        <spam className="txt" style={{color:"#478DF7"}}>Новый трек в редакторе</spam>
+                            <spam className="txt" style={{ color: "#478DF7" }}>Новый трек в редакторе</spam>
                         </button>
-                   </div>
-                   <div className="own-sound">
-                     <button className="btn2">
-                        <div className="bl">
-                            <img className="chevron" src={"icons/add-circle.svg"} alt="My SVG" />
-                        </div>
-                        <div className="br">
-                        <span className='txt' onClick={handleMenuSelectUp} style={{color:"black", textAlign:"center", alignItems:"center",justifyContent:"center"}}>Загрузить новый трек</span>
-                        </div>
-                     </button>
-                   </div>
+                    </div>
+                    <div className="own-sound">
+                        <button className="btn2">
+                            <div className="bl">
+                                <img className="chevron" src={"icons/add-circle.svg"} alt="" />
+                            </div>
+                            <div className="br">
+                                <span className='txt' onClick={handleMenuSelectUp} style={{ color: "black", textAlign: "center", alignItems: "center", justifyContent: "center" }}>Загрузить новый трек</span>
+                            </div>
+                        </button>
+                    </div>
                 </ul>
             </nav>
         </div>
